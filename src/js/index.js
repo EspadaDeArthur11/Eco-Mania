e8c622913023bb2d65eb1ffb0cc7b568d077b90e
EspadaDeArthur11 <felipe.schulze@hotmail.com>
2025-10-23 10:50:57 -0300

    Implementação da funcionalidade do menu

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("botaoJogar").onclick = function() {
+    window.location.href = "game.html";
+}
+
+document.getElementById("botaoRec").onclick = function() {
+    window.location.href = "records.html";
+}
