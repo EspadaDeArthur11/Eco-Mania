1656c219978f0e98c4542e092d5a4bed6eedc35d
grmsBR <guilherme.ramos.co@gmail.com>
2025-10-28 18:23:55 -0300

    Update firebase.js

---
+++ src/js/firebase.js
@@ -0,0 +1,6 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCoYDEOTGJeOuuGKSqamAflKT8Y7HzPhOs",
+  authDomain: "ecomania-81cfa.firebaseapp.com",
+  projectId: "ecomania-81cfa",
+  appId: "1:870049766827:web:f1d1738b12884f9fe446ee",
+};
