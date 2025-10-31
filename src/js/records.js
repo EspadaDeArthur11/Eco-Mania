abfa1bbfa5636c5670fe122f6b47750c6cffb2d4
grmsBR <guilherme.ramos.co@gmail.com>
2025-10-31 00:03:00 -0300

    Update records.js

--- src/js/records.js
+++ src/js/records.js
@@ -1,4 +1,7 @@
+// Preenche a tabela de recordes em tempo real (top 10)
 PhaserDB.attachLiveTable(10);
-document.getElementById("botaoMenu").addEventListener("click", () => {
+
+// Voltar ao menu
+document.getElementById("botaoMenu")?.addEventListener("click", () => {
   window.location.href = "index.html";
 });
