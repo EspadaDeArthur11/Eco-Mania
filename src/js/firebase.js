c210be9283e443f121bce1eac0a44a4bd74283b3
grmsBR <guilherme.ramos.co@gmail.com>
2025-10-30 23:52:39 -0300

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,16 @@
+
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCoYDEOTGJeOuuGKSqamAflKT8Y7HzPhOs",
   authDomain: "ecomania-81cfa.firebaseapp.com",
   projectId: "ecomania-81cfa",
+  storageBucket: "ecomania-81cfa.appspot.com",
   appId: "1:870049766827:web:f1d1738b12884f9fe446ee",
 };
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(FIREBASE_CONFIG);
+}
+
+const db = firebase.firestore();
+console.log("🔥 Firebase inicializado");
+
