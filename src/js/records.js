f20d66e3d22c82d5d36dd313ad9b57450b0ac44e
grmsBR <guilherme.ramos.co@gmail.com>
2025-10-30 23:36:17 -0300

    Update records.js

--- src/js/records.js
+++ src/js/records.js
@@ -1,9 +1,4 @@
-document.getElementById("botaoMenu").onclick = function() {
-    window.location.href = "index.html";
-}
-// Preenche a tabela de recordes ao vivo (Firestore) / 1x (localStorage fallback)
 PhaserDB.attachLiveTable(10);
-
 document.getElementById("botaoMenu").addEventListener("click", () => {
   window.location.href = "index.html";
 });
